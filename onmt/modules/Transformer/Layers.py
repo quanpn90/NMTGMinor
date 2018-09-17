6f3701221c4a66572e4cffe1eac86f45a65abaf8
Quan Pham <ngoc.pham@kit.edu>
2018-09-17 14:14:00 +0200

    Attention with time dimension first

--- onmt/modules/Transformer/Layers.py
+++ onmt/modules/Transformer/Layers.py
@@ -191,10 +191,11 @@ class MultiHeadAttention(nn.Module):
         
     """
     
-    def __init__(self, h, d_model, attn_p=0.1, static=True):
+    def __init__(self, h, d_model, attn_p=0.1, static=True, share=2):
         super(MultiHeadAttention, self).__init__()      
         self.h = h
         self.d = d_model
+        self.share = share
         
         assert d_model % h == 0
         
@@ -227,92 +228,148 @@ class MultiHeadAttention(nn.Module):
                 .transpose(1, 2)
     
 
+    def forward_bf(self, query, key, value, mask, query_mask=None, value_mask=None):
+        b, len_query = query.size(0), query.size(1)
+        len_key = key.size(1)
+        
+        key_mask = value_mask
+        
+        # project inputs to multi-heads
+        proj_query = self.fc_query(query, mask=query_mask)       # batch_size x len_query x h*d_head
+        proj_key   = self.fc_key(key, mask=key_mask)             # batch_size x len_key x h*d_head
+        proj_value = self.fc_value(value, mask=value_mask)       # batch_size x len_key x h*d_head
+        
+        # prepare the shape for applying softmax
+        proj_query = self._prepare_proj(proj_query)  # batch_size*h x len_query x d_head
+        proj_key = self._prepare_proj(proj_key)           # batch_size*h x len_key x d_head
+        proj_value = self._prepare_proj(proj_value)       # batch_size*h x len_key x d_head
+        
+        proj_query = proj_query * (self.d_head**-0.5)
+        
+        # get dotproduct softmax attns for each head
+        attns = torch.bmm(proj_query, proj_key.transpose(1,2))  # batch_size*h x len_query x len_key
+        
+        attns = attns.view(b, self.h, len_query, len_key) 
+        if isinstance(mask, Variable):
+            mask_ = mask.unsqueeze(-3)
+        elif torch.is_tensor(mask):
+            mask_ = Variable(mask.unsqueeze(-3))    
+        # FP16 support: cast to float and back
+        attns = attns.float().masked_fill_(mask_, -float('inf')).type_as(attns)
+        attns = self.sm(attns)
+        # return mean attention from all heads as coverage 
+        coverage = torch.mean(attns, dim=1) 
+        attns = self.attn_dropout(attns)
+        attns = attns.view(b*self.h, len_query, len_key)
+        
+        # apply attns on value
+        out = torch.bmm(attns, proj_value)      # batch_size*h x len_query x d_head
+        out = contiguous(out.view(b, self.h, len_query, self.d_head).transpose(1,2))
+        out = out.view(b, len_query, self.h*self.d_head)
+            
+        out = self.fc_concat(out, mask=query_mask)
+       
+        return out, coverage
+        
     def forward(self, query, key, value, mask, query_mask=None, value_mask=None):
-        b, len_query = query.size(0), query.size(1)
-        len_key = key.size(1)
+        
+        query = query.transpose(0, 1)
+        key = key.transpose(0, 1)
+        value = value.transpose(0, 1)
+        len_query, b = query.size(0), query.size(1)
+        len_key,  b_ = key.size(0), key.size(1)
         
         key_mask = value_mask
         
+        proj_query = self.fc_query(query, mask=query_mask)   # batch_size*h x len_query x d_head
         # project inputs to multi-heads
-        proj_query = self.fc_query(query, mask=query_mask)       # batch_size x len_query x h*d_head
-        proj_key   = self.fc_key(key, mask=key_mask)             # batch_size x len_key x h*d_head
-        proj_value = self.fc_value(value, mask=value_mask)       # batch_size x len_key x h*d_head
+        if self.share == 1:
+            proj_key   = self.fc_key(key, mask=key_mask)             # batch_size x len_key x h*d_head
+            proj_value = self.fc_value(value, mask=value_mask)       # batch_size x len_key x h*d_head
+        elif self.share == 2:
+            #~ proj_query = self.fc_query(query)      # batch_size x len_query x h*d_head
+            #~ shared_kv = self.fc_kv(key)
+            #~ proj_key, proj_value = shared_kv.chunk(2, dim=-1)
+            proj_key   = self.fc_key(key, mask=key_mask)             # batch_size x len_key x h*d_head
+            proj_value = self.fc_value(value, mask=value_mask)       # batch_size x len_key x h*d_head
+        else:
+            raise NotImplementedError
+        
+        q, k, v = proj_query, proj_key, proj_value
         
         # prepare the shape for applying softmax
-        proj_query = self._prepare_proj(proj_query)  # batch_size*h x len_query x d_head
-        proj_key = self._prepare_proj(proj_key)           # batch_size*h x len_key x d_head
-        proj_value = self._prepare_proj(proj_value)       # batch_size*h x len_key x d_head
+        q = q.contiguous().view(len_query, b*self.h, self.d_head).transpose(0, 1)
+        k = k.contiguous().view(len_key,   b*self.h, self.d_head).transpose(0, 1)
+        v = v.contiguous().view(len_key,   b*self.h, self.d_head).transpose(0, 1)
         
-        proj_query = proj_query * (self.d_head**-0.5)
+        q = q * (self.d_head**-0.5)
         
         # get dotproduct softmax attns for each head
-        attns = torch.bmm(proj_query, proj_key.transpose(1,2))  # batch_size*h x len_query x len_key
+        attns = torch.bmm(q, k.transpose(1,2))  # batch_size*h x len_query x len_key
         
         attns = attns.view(b, self.h, len_query, len_key) 
-        if isinstance(mask, Variable):
-            mask_ = mask.unsqueeze(-3)
-        elif torch.is_tensor(mask):
-            mask_ = Variable(mask.unsqueeze(-3))    
+        mask_ = mask.unsqueeze(-3)
         # FP16 support: cast to float and back
         attns = attns.float().masked_fill_(mask_, -float('inf')).type_as(attns)
-        attns = self.sm(attns)
+        attns = F.softmax(attns.float(), dim=-1).type_as(attns)
         # return mean attention from all heads as coverage 
         coverage = torch.mean(attns, dim=1) 
         attns = self.attn_dropout(attns)
         attns = attns.view(b*self.h, len_query, len_key)
         
         # apply attns on value
-        out = torch.bmm(attns, proj_value)      # batch_size*h x len_query x d_head
-        out = contiguous(out.view(b, self.h, len_query, self.d_head).transpose(1,2))
-        out = out.view(b, len_query, self.h*self.d_head)
+        out = torch.bmm(attns, v)      # batch_size*h x len_query x d_head
+        out = out.transpose(0, 1).contiguous().view(len_query, b, self.d)
             
-        out = self.fc_concat(out, mask=query_mask)
+        out = self.fc_concat(out)
+        
+        out = out.transpose(0, 1).contiguous()
        
         return out, coverage
         
         
-    def step(self, query, key, value, mask, query_mask=None, value_mask=None):
-        b, len_query = query.size(0), query.size(1)
-        len_key = key.size(1)
-        
-        key_mask = value_mask
-        
-        # project inputs to multi-heads
-        proj_query = self.fc_query(query, mask=query_mask)       # batch_size x len_query x h*d_head
-        proj_key   = self.fc_key(key, mask=key_mask)             # batch_size x len_key x h*d_head
-        proj_value = self.fc_value(value, mask=value_mask)       # batch_size x len_key x h*d_head
-        
-        # prepare the shape for applying softmax
-        proj_query = self._prepare_proj(proj_query)  # batch_size*h x len_query x d_head
-        proj_key = self._prepare_proj(proj_key)           # batch_size*h x len_key x d_head
-        proj_value = self._prepare_proj(proj_value)       # batch_size*h x len_key x d_head
-        
-        proj_query = proj_query * (self.d_head**-0.5)
-        
-        # get dotproduct softmax attns for each head
-        attns = torch.bmm(proj_query, proj_key.transpose(1,2))  # batch_size*h x len_query x len_key
-        
-        attns = attns.view(b, self.h, len_query, len_key) 
-        if isinstance(mask, Variable):
-            mask_ = mask.unsqueeze(-3)
-        elif torch.is_tensor(mask):
-            mask_ = Variable(mask.unsqueeze(-3))    
-        # FP16 support: cast to float and back
-        attns = attns.float().masked_fill_(mask_, -float('inf')).type_as(attns)
-        attns = self.sm(attns)
-        # return mean attention from all heads as coverage 
-        coverage = torch.mean(attns, dim=1) 
-        attns = self.attn_dropout(attns)
-        attns = attns.view(b*self.h, len_query, len_key)
-        
-        # apply attns on value
-        out = torch.bmm(attns, proj_value)      # batch_size*h x len_query x d_head
-        out = contiguous(out.view(b, self.h, len_query, self.d_head).transpose(1,2))
-        out = out.view(b, len_query, self.h*self.d_head)
-            
-        out = self.fc_concat(out, mask=query_mask)
-       
-        return out, coverage
+    #~ def step(self, query, key, value, mask, query_mask=None, value_mask=None):
+        #~ b, len_query = query.size(0), query.size(1)
+        #~ len_key = key.size(1)
+        #~ 
+        #~ key_mask = value_mask
+        #~ 
+        #~ # project inputs to multi-heads
+        #~ proj_query = self.fc_query(query)       # batch_size x len_query x h*d_head
+        #~ proj_key   = self.fc_key(key)             # batch_size x len_key x h*d_head
+        #~ proj_value = self.fc_value(value)       # batch_size x len_key x h*d_head
+        #~ 
+        #~ # prepare the shape for applying softmax
+        #~ proj_query = self._prepare_proj(proj_query)  # batch_size*h x len_query x d_head
+        #~ proj_key = self._prepare_proj(proj_key)           # batch_size*h x len_key x d_head
+        #~ proj_value = self._prepare_proj(proj_value)       # batch_size*h x len_key x d_head
+        #~ 
+        #~ proj_query = proj_query * (self.d_head**-0.5)
+        #~ 
+        #~ # get dotproduct softmax attns for each head
+        #~ attns = torch.bmm(proj_query, proj_key.transpose(1,2))  # batch_size*h x len_query x len_key
+        #~ 
+        #~ attns = attns.view(b, self.h, len_query, len_key) 
+        #~ if isinstance(mask, Variable):
+            #~ mask_ = mask.unsqueeze(-3)
+        #~ elif torch.is_tensor(mask):
+            #~ mask_ = Variable(mask.unsqueeze(-3))    
+        #~ # FP16 support: cast to float and back
+        #~ attns = attns.float().masked_fill_(mask_, -float('inf')).type_as(attns)
+        #~ attns = self.sm(attns)
+        #~ # return mean attention from all heads as coverage 
+        #~ coverage = torch.mean(attns, dim=1) 
+        #~ attns = self.attn_dropout(attns)
+        #~ attns = attns.view(b*self.h, len_query, len_key)
+        #~ 
+        #~ # apply attns on value
+        #~ out = torch.bmm(attns, proj_value)      # batch_size*h x len_query x d_head
+        #~ out = contiguous(out.view(b, self.h, len_query, self.d_head).transpose(1,2))
+        #~ out = out.view(b, len_query, self.h*self.d_head)
+            #~ 
+        #~ out = self.fc_concat(out)
+       #~ 
+        #~ return out, coverage
     
 class FeedForward(nn.Module):
     """Applies position-wise feed forward to inputs
@@ -384,7 +441,7 @@ class EncoderLayer(nn.Module):
         self.postprocess_attn = PrePostProcessing(d_model, p, sequence='da', static=onmt.Constants.static)
         self.preprocess_ffn = PrePostProcessing(d_model, p, sequence='n')
         self.postprocess_ffn = PrePostProcessing(d_model, p, sequence='da', static=onmt.Constants.static)
-        self.multihead = MultiHeadAttention(h, d_model, attn_p=attn_p, static=onmt.Constants.static)
+        self.multihead = MultiHeadAttention(h, d_model, attn_p=attn_p, static=onmt.Constants.static, share=2)
         
         if onmt.Constants.activation_layer == 'linear_relu_linear':
             ff_p = p
@@ -453,8 +510,8 @@ class DecoderLayer(nn.Module):
         self.postprocess_ffn = PrePostProcessing(d_model, p, sequence='da', static=onmt.Constants.static)
         
         
-        self.multihead_tgt = MultiHeadAttention(h, d_model, attn_p=attn_p, static=onmt.Constants.static)
-        self.multihead_src = MultiHeadAttention(h, d_model, attn_p=attn_p, static=onmt.Constants.static)
+        self.multihead_tgt = MultiHeadAttention(h, d_model, attn_p=attn_p, static=onmt.Constants.static, share=1)
+        self.multihead_src = MultiHeadAttention(h, d_model, attn_p=attn_p, static=onmt.Constants.static, share=2)
         
         if onmt.Constants.activation_layer == 'linear_relu_linear':
             ff_p = p
@@ -492,7 +549,7 @@ class DecoderLayer(nn.Module):
         """
         
         query = self.preprocess_src_attn(input)
-        out, coverage = self.multihead_src.step(query, context, context, mask_src)
+        out, coverage = self.multihead_src(query, context, context, mask_src)
         input = self.postprocess_src_attn(out, input)
         
         """ Feed forward layer 
