995a214cd5d11d40d881b44ca13ea14e6a32ab08
Quan Pham <ngoc.pham@kit.edu>
2018-12-12 13:30:01 +0100

    Cleaning up for better publishing

--- onmt/modules/__init__.py
+++ onmt/modules/__init__.py
@@ -1,7 +1,6 @@
 from onmt.modules.GlobalAttention import GlobalAttention
-from onmt.modules.ImageEncoder import ImageEncoder
 from onmt.modules.BaseModel import Generator, NMTModel
 from onmt.modules.StaticDropout import StaticDropout
 
 # For flake8 compatibility.
-__all__ = [GlobalAttention, ImageEncoder, Generator, NMTModel, StaticDropout]
+__all__ = [GlobalAttention, Generator, NMTModel, StaticDropout]
