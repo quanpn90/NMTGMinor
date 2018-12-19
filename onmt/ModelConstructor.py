Fixed numerical problem for Normal KL divergence

onmt/ModelConstructor.py
@@ -109,7 +109,7 @@ def build_model(opt, dicts):
         
         positional_encoder = PositionalEncoding(opt.model_size, len_max=MAX_LEN)
         
-        encoder = StochasticTransformerEncoder(opt, dicts['src'], positional_encoder)
+        encoder = StochasticTransformerEncoder(opt, embedding_src, positional_encoder)
         
         decoder = StochasticTransformerDecoder(opt, dicts['tgt'], positional_encoder)
         
@@ -128,8 +128,8 @@ def build_model(opt, dicts):
         
         positional_encoder = PositionalEncoding(opt.model_size, len_max=MAX_LEN )
         
-        encoder = FCTransformerEncoder(opt, dicts['src'], positional_encoder)
-        decoder = FCTransformerDecoder(opt, dicts['tgt'], positional_encoder)
+        encoder = FCTransformerEncoder(opt, embedding_src, positional_encoder)
+        decoder = FCTransformerDecoder(opt, embedding_tgt, positional_encoder)
         
         generator = onmt.modules.BaseModel.Generator(opt.model_size, dicts['tgt'].size())
         
@@ -145,8 +145,8 @@ def build_model(opt, dicts):
         
         positional_encoder = PositionalEncoding(opt.model_size, len_max=MAX_LEN )
         
-        encoder = ParallelTransformerEncoder(opt, dicts['src'], positional_encoder)
-        decoder = ParallelTransformerDecoder(opt, dicts['tgt'], positional_encoder)
+        encoder = ParallelTransformerEncoder(opt, embedding_src, positional_encoder)
+        decoder = ParallelTransformerDecoder(opt, embedding_tgt, positional_encoder)
         
         generator = onmt.modules.BaseModel.Generator(opt.model_size, dicts['tgt'].size())
         
@@ -166,8 +166,8 @@ def build_model(opt, dicts):
         time_encoder = TimeEncoding(opt.model_size, len_max=32)
 
         
-        encoder = UniversalTransformerEncoder(opt, dicts['src'], positional_encoder, time_encoder)
-        decoder = UniversalTransformerDecoder(opt, dicts['tgt'], positional_encoder, time_encoder)
+        encoder = UniversalTransformerEncoder(opt, embedding_src, positional_encoder, time_encoder)
+        decoder = UniversalTransformerDecoder(opt, embedding_tgt, positional_encoder, time_encoder)
         
         generator = onmt.modules.BaseModel.Generator(opt.model_size, dicts['tgt'].size())
         
@@ -202,8 +202,8 @@ def build_model(opt, dicts):
         from onmt.modules.MixtureModel.Models import MixtureEncoder, MixtureDecoder
         positional_encoder = PositionalEncoding(opt.model_size, len_max=MAX_LEN)
         
-        encoder = MixtureEncoder(opt, dicts['src'], positional_encoder)
-        decoder = MixtureDecoder(opt, dicts['tgt'], positional_encoder)
+        encoder = MixtureEncoder(opt, embedding_src, positional_encoder)
+        decoder = MixtureDecoder(opt, embedding_tgt, positional_encoder)
 
         generator = onmt.modules.BaseModel.Generator(opt.model_size, dicts['tgt'].size())
 
@@ -216,13 +216,7 @@ def build_model(opt, dicts):
     if opt.tie_weights:  
         print("* Joining the weights of decoder input and output embeddings")
         model.tie_weights()
-       
-    
 
-    
-
-
-        
     init = torch.nn.init
         
     init.xavier_uniform_(model.generator.linear.weight)

onmt/modules/VariationalTransformer/Inference.py
@@ -39,9 +39,10 @@ def mean_with_mask(context, mask):
     # mask dimension: T x B x 1 (with unsqueeze)
     # first, we have to mask the context with zeros at the unwanted position
 
-    context.masked_fill_(mask, 0)
+    eps = 0
+    context.masked_fill_(mask, eps)
 
-    # then take the sum over the dimension
+    # then take the sum over the time dimension
     context_sum = torch.sum(context, dim=0, keepdim=False)
 
 
@@ -53,8 +54,6 @@ def mean_with_mask(context, mask):
 
 """
     The Prior model estimates p(z | x)
-
-
 """
 class NeuralPrior(nn.Module):
 
@@ -78,7 +77,6 @@ class NeuralPrior(nn.Module):
         self.encoder = TransformerEncoder(encoder_opt, embedding, positional_encoder)
 
         self.projector = Linear(opt.model_size, opt.model_size)
-        # self.norm = nn.LayerNorm(opt.model_size)
         self.mean_predictor = Linear(opt.model_size, opt.model_size)
         self.var_predictor = Linear(opt.model_size, opt.model_size)
 
@@ -102,7 +100,7 @@ class NeuralPrior(nn.Module):
 
         context = mean_with_mask(context, mask)
         encoder_meaning = context
-        context = torch.tanh(self.projector(context))
+        context = F.tanh(self.projector(context))
        
 
         mean = self.mean_predictor(context)       
@@ -172,8 +170,7 @@ class NeuralPosterior(nn.Module):
 
         context = torch.cat([encoder_context, decoder_context], dim=-1)
 
-        context = torch.tanh(self.projector(context))
-
+        context = F.tanh(self.projector(context))
 
         mean = self.mean_predictor(context)
         var = torch.nn.functional.softplus(self.var_predictor(context))

onmt/modules/VariationalTransformer/Models.py
@@ -12,6 +12,7 @@ from onmt.modules.WordDrop import embedded_dropout
 from collections import defaultdict
 
 from onmt.modules.Linear import XavierLinear as Linear
+from onmt.modules.KLDivergence import kl_divergence_normal
 
 torch.set_printoptions(threshold=10000)
 
@@ -98,12 +99,6 @@ class VariationalDecoder(TransformerDecoder):
         output_plus_z = torch.cat([z, output], dim=0) # concat to the time dimension
 
         output = output_plus_z
-        # print(output.size())
-        # output_plus_z = torch.cat([output, z], dim=-1)
-
-        # combine input with latent variable
-        # output = self.projector(output_plus_z))
-
         
         # the rest will be the same as the Transformer
 
@@ -279,7 +274,8 @@ class VariationalTransformer(NMTModel):
         decoder_output, coverage = self.decoder(tgt, encoder_context, src, z)
 
         # compute KL between prior and posterior
-        kl_divergence = torch.distributions.kl.kl_divergence(q_z, p_z)
+        # kl_divergence = torch.distributions.kl.kl_divergence(q_z, p_z)
+        kl_divergence = kl_divergence_normal(q_z, p_z)
         outputs = defaultdict(lambda: None)
 
 