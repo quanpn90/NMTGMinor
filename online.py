f3bbdbc0c5033df191e89b3546a56d0b93155595
Quan Pham Ngoc <quanpn90@gmail.com>
2019-06-05 16:42:05 +0200

    Fix online translator path

---
+++ online.py
@@ -0,0 +1,18 @@
+import os
+from onmt.OnlineTranslator import TranslatorParameter, OnlineTranslator
+import sys
+
+
+filename="/model/model.conf"
+
+t = OnlineTranslator(filename)
+print("NMT initialized")
+sys.stdout.flush()
+
+while True:
+#    sys.stderr.write("Waiting for data\n");
+    line = sys.stdin.readline();
+#    sys.stderr.write("Input: "+line+"\n");
+    print(t.translate(line))
+#    sys.stderr.write("Translation done\n");
+    sys.stdout.flush()
