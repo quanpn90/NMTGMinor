1effd5e8ec50093a64583b22a831659d7c13c578
Quan Pham Ngoc <quanpn90@gmail.com>
2019-06-24 11:56:19 +0200

    Fixed loading checkpoint

--- train.py
+++ train.py
@@ -100,8 +100,9 @@ def main():
     else:
         raise NotImplementedError
 
-    if opt.load_from is not None:
-        checkpoint = torch.load(opt.load_from, map_location=lambda storage, loc: storage)
+    # if opt.load_from is not None:
+    if not opt.load_from:
+        checkpoint = torch.load(opt#.load_from, map_location=lambda storage, loc: storage)
         print("* Loading dictionaries from the checkpoint")
         dicts = checkpoint['dicts']
     else:
