Added O1 optimization to fix loading / overflow with O2

onmt/model_factory.py
@@ -242,6 +242,8 @@ def init_model_parameters(model, opt):
 
 
 def build_language_model(opt, dicts):
+    opt = backward_compatible(opt)
+
     onmt.constants.layer_norm = opt.layer_norm
     onmt.constants.weight_norm = opt.weight_norm
     onmt.constants.activation_layer = opt.activation_layer
@@ -249,22 +251,29 @@ def build_language_model(opt, dicts):
     onmt.constants.attention_out = opt.attention_out
     onmt.constants.residual_type = opt.residual_type
 
-    from onmt.legacy.LSTMLM.Models import LSTMLMDecoder, LSTMLM
+    # from onmt.legacy.LSTMLM.Models import LSTMLMDecoder, LSTMLM
+    from onmt.models.transformer_xl import TransformerXL
 
-    decoder = LSTMLMDecoder(opt, dicts['tgt'])
+    # decoder = LSTMLMDecoder(opt, dicts['tgt'])
 
+    embedding_tgt = nn.Embedding(dicts['tgt'].size(),
+                                 opt.model_size,
+                                 padding_idx=onmt.constants.PAD)
+    if opt.use_language_embedding:
+        print("* Create language embeddings with %d languages" % len(dicts['langs']))
+        language_embeddings = nn.Embedding(len(dicts['langs']), opt.model_size)
+    else:
+        language_embeddings = None
+    #
     generators = [onmt.modules.base_seq2seq.Generator(opt.model_size, dicts['tgt'].size())]
 
-    model = LSTMLM(None, decoder, nn.ModuleList(generators))
+    model = TransformerXL(opt, embedding_tgt, nn.ModuleList(generators), language_embeddings=language_embeddings)
+
+    model.tgt_dict = dicts['tgt']
 
     if opt.tie_weights:
         print("* Joining the weights of decoder input and output embeddings")
         model.tie_weights()
-
-    for g in model.generator:
-        init.xavier_uniform_(g.linear.weight)
-
-    init.normal_(model.decoder.word_lut.weight, mean=0, std=opt.model_size ** -0.5)
 
     return model
 

onmt/models/transformers.py
@@ -170,6 +170,7 @@ class TransformerEncoder(nn.Module):
                 input = input.narrow(2, 1, input.size(2) - 1)
                 emb = self.audio_trans(input.contiguous().view(-1, input.size(2))).view(input.size(0),
                                                                                         input.size(1), -1)
+                emb = emb.type_as(input)
             else:
                 long_mask = input.narrow(2, 0, 1).squeeze(2).eq(onmt.constants.PAD)
                 input = input.narrow(2, 1, input.size(2) - 1)
@@ -211,7 +212,6 @@ class TransformerEncoder(nn.Module):
         context = self.preprocess_layer(context)
 
         for i, layer in enumerate(self.layer_modules):
-
                 context = layer(context, mask_src)  # batch_size x len_src x d_model
 
         # From Google T2T

onmt/train_utils/trainer.py
@@ -31,7 +31,7 @@ class BaseTrainer(object):
 
         self.additional_data = []
 
-    def add_additional_data(self,d,ratio):
+    def add_additional_data(self, d, ratio):
         self.additional_data = d
         if ratio == "-1" :
             self.additional_data_ratio = [1]*(len(self.additional_data + 1))
@@ -103,14 +103,23 @@ class XETrainer(BaseTrainer):
             self.optim = onmt.Optim(opt)
             self.optim.set_parameters(self.model.parameters())
 
-            opt_level = "O0" if not self.opt.fp16 else "O2"
+            if not self.opt.fp16:
+                opt_level = "O0"
+                keep_batchnorm_fp32 = False
+            elif self.opt.fp16_mixed:
+                opt_level = "O1"
+                keep_batchnorm_fp32 = None
+            else:
+                opt_level = "O2"
+                keep_batchnorm_fp32 = False
 
             if self.cuda:
                 self.model, self.optim.optimizer = amp.initialize(self.model,
                                                                   self.optim.optimizer,
                                                                   opt_level=opt_level,
-                                                                  keep_batchnorm_fp32=False, loss_scale="dynamic",
-                                                                  verbosity=0)
+                                                                  keep_batchnorm_fp32=keep_batchnorm_fp32,
+                                                                  loss_scale="dynamic",
+                                                                  verbosity=1)
         # An ugly hack to switch between align right and align left
         if hasattr(self.model, 'relative'):
             if self.model.relative:
@@ -141,7 +150,7 @@ class XETrainer(BaseTrainer):
                 'additional_data_iteration' : getattr(self, 'additional_data_iteration', None),
                 'amp': amp.state_dict()
         }
-        
+            
         file_name = '%s_ppl_%.6f_e%.2f.pt' % (opt.save_model, valid_ppl, epoch)
         print('Writing to %s' % file_name)
         torch.save(checkpoint, file_name)
@@ -180,7 +189,7 @@ class XETrainer(BaseTrainer):
                 #     streaming_state = None
 
                 if self.cuda:
-                    batch.cuda(fp16=self.opt.fp16)
+                    batch.cuda(fp16=self.opt.fp16 and not self.opt.fp16_mixed)
                 
                 """ outputs can be either 
                         hidden states from decoder or
@@ -264,7 +273,7 @@ class XETrainer(BaseTrainer):
             for b in range(len(batches)):
                 batch = batches[b]
                 if self.cuda:
-                    batch.cuda(fp16=self.opt.fp16)
+                    batch.cuda(fp16=self.opt.fp16 and not self.opt.fp16_mixed)
 
                 # if opt.streaming:
                 #     if train_data.is_new_stream():
@@ -399,11 +408,17 @@ class XETrainer(BaseTrainer):
         
         if checkpoint is not None:
             self.model.load_state_dict(checkpoint['model'])
+            prev_opt = checkpoint['opt'] if 'opt' in checkpoint else None
             
             if not opt.reset_optim:
                 self.optim.load_state_dict(checkpoint['optim'])
-                if 'amp' in checkpoint:
-                    amp.load_state_dict(checkpoint['amp'])
+                if prev_opt is not None and hasattr(prev_opt, "fp16_mixed"):
+                    # Only load amp information if the mode is the same
+                    # Maybe its better to change between optimization mode?
+                    if opt.fp16_mixed == prev_opt.fp16_mixed and opt.fp16 == prev_opt.fp16:
+                        if 'amp' in checkpoint:
+                            amp.load_state_dict(checkpoint['amp'])
+
                 if 'batch_order' in checkpoint:
                     batch_order = checkpoint['batch_order']
                     iteration = checkpoint['iteration'] + 1
@@ -412,7 +427,7 @@ class XETrainer(BaseTrainer):
                     iteration = 0
                 opt.start_epoch = int(math.floor(float(checkpoint['epoch'] + 1)))
 
-                resume=True
+                resume = True
                 if len(self.additional_data) > 0:
                     if 'additional_batch_order' in checkpoint:
                         self.additional_batch_order = checkpoint['additional_batch_order']
@@ -422,7 +437,7 @@ class XETrainer(BaseTrainer):
             else:
                 batch_order = None
                 iteration = 0
-                resume=False
+                resume = False
                 self.init_additional_data()
 
             del checkpoint['model']