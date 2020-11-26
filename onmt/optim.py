Fixed saving/loading and check nan gradients in distributed training

onmt/optim.py
@@ -319,14 +319,14 @@ def normalize_gradients(parameters, denom):
         p.grad.data.div_(denom)
 
 
-def detech_nan(parameters):
+def detech_nan_inf(parameters):
     parameters = list(filter(lambda p: p.grad is not None, parameters))
 
     for p in parameters:
-        if torch.equal(p.grad.data, p.grad.data):
-            continue
-        else:
+        if torch.isinf(p.grad.data).any() or torch.isnan(p.grad.data).any():
             return True
+        else:
+            continue
 
     return False
 
@@ -374,7 +374,10 @@ class Optim(object):
         self.params = list(params_)  # careful: params may be a generator
         if self.method == 'sgd':
             self.optimizer = optim.SGD(self.params, lr=self.lr, weight_decay=self.weight_decay, momentum=0.0)
-        elif self.method in ['adam', 'fused_adam']:
+        elif self.method == 'adam':
+            self.optimizer = optim.Adam(self.params, lr=self.lr, betas=(self.beta1, self.beta2), eps=1e-9,
+                                        weight_decay=self.weight_decay, amsgrad=self.amsgrad)
+        elif self.method in ['fused_adam']:
 
             fast_adam = True
             try:
@@ -449,15 +452,19 @@ class Optim(object):
         # grad_norm = clip_grad_norm(self.params, self.max_grad_norm).item()
 
         overflow = False
-        if hasattr(self.optimizer,"_amp_stash.already_patched") and self.optimizer._amp_stash.already_patched:
+        if hasattr(self.optimizer, "_amp_stash.already_patched") and self.optimizer._amp_stash.already_patched:
             overflow = True
+
+        # if gradients have NaN/inf: return (which will be zeroed afterwards)
+        if detech_nan_inf(self.params):
+            return
+
         "Automatically scale learning rate over learning period if not overflow"
         if not overflow:
             self._step += 1
             if 'noam' in self.update_method or 'cosine' in self.update_method:
                 self.updateLearningRate()
 
-        # For
         if scaler is not None:
             scaler.step(self.optimizer)
         else:

onmt/speech/ctc_loss.py
@@ -30,7 +30,7 @@ class CTC(torch.nn.Module):
 
         if self.ctc_type == "builtin":
             reduction_type = "sum" if reduce else "none"
-            self.ctc_loss = torch.nn.CTCLoss(reduction=reduction_type)
+            self.ctc_loss = torch.nn.CTCLoss(blank=onmt.constants.PAD, reduction=reduction_type, zero_infinity=True)
 
         elif self.ctc_type == "warpctc":
             import warpctc_pytorch as warp_ctc
@@ -88,6 +88,12 @@ class CTC(torch.nn.Module):
             input_lengths = (1 - source_mask).squeeze(1).sum(1)
         else:
             input_lengths = (1 - source_mask).sum(1)
+
+        # print("MAX SOURCE LENGTH", logits.size(0), logits.size())
+        # print(input_lengths)
+
+        # print("MAX LENGTH", targets.size(0), targets.size())
+        # print(target_lengths)
 
         if self.ctc_type == 'builtin':
             targets = targets.transpose(0, 1)

onmt/train_utils/mp_trainer.py
@@ -24,6 +24,7 @@ from onmt.train_utils.stats import Logger
 from onmt.utils import checkpoint_paths, normalize_gradients
 from onmt.model_factory import build_model, optimize_model, init_model_parameters
 import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP_model
 from torch.cuda.amp import autocast
 import warnings
 
@@ -211,11 +212,17 @@ class Trainer(object):
             if not opt.memory_profiling:
                 # distributed is required to convert BatchNorm to SyncBatchNorm for DDP
                 optimize_model(model, distributed=(self.world_size > 1))
-                # optimize_model(model)
 
         init_model_parameters(model, opt)
         self.model = model
         self.loss_function = loss_function
+        self.grad_scaler = torch.cuda.amp.GradScaler()
+
+        if opt.load_from:
+            checkpoint = torch.load(opt.load_from, map_location=lambda storage, loc: storage)
+            self.model.load_state_dict(checkpoint['model'])
+            if 'scaler' in checkpoint and checkpoint['scaler'] is not None:
+                self.grad_scaler.load_state_dict(checkpoint['scaler'])
 
         if self.cuda:
             torch.cuda.set_device(self.device)
@@ -252,16 +259,22 @@ class Trainer(object):
             if self.is_main():
                 print("[INFO] Optimizer: ", self.optim.optimizer)
 
-            if self.world_size > 1:
-                # find_unused_parameters may be required for dropped layer (parameters that are not connected to
-                # any particular graph)
-                find_unused_parameters = True if opt.death_rate > 0.0 else False
+            if opt.load_from:
+                if 'optim' in checkpoint and checkpoint['optim'] is not None and not opt.reset_optim:
+                    self.optim.load_state_dict(checkpoint['optim'])
 
-                self.model = torch.nn.parallel.DistributedDataParallel(self.model, device_ids=[self.rank],
-                                                                       output_device=self.rank,
-                                                                       find_unused_parameters=find_unused_parameters)
+        if self.world_size > 1:
+            # find_unused_parameters may be required for dropped layer (parameters that are not connected to
+            # any particular graph)
+            find_unused_parameters = True if opt.death_rate > 0.0 else False
 
-            self.grad_scaler = torch.cuda.amp.GradScaler()
+            self.model = torch.nn.parallel.DistributedDataParallel(self.model, device_ids=[self.rank],
+                                                                   output_device=self.rank,
+                                                                   find_unused_parameters=find_unused_parameters)
+
+        # if opt.load_from:
+        #     print("[INFO] Process %d loading parameters..." % self.rank, flush=True)
+
 
         print("[INFO] Process %d ready." % self.rank, flush=True)
 
@@ -487,7 +500,10 @@ class Trainer(object):
         model = self.model
         dicts = self.dicts
 
-        model_state_dict = self.model.state_dict()
+        if isinstance(model, torch.nn.parallel.DistributedDataParallel):
+            model_state_dict = self.model.module.state_dict()
+        else:
+            model_state_dict = self.model.state_dict()
         optim_state_dict = self.optim.state_dict()
 
         if itr:
@@ -597,6 +613,7 @@ class Trainer(object):
         total_tokens, total_loss, total_words = zero_tensor(), zero_tensor(), zero_tensor()
         total_non_pads = zero_tensor()
         report_loss, report_tgt_words = zero_tensor(), zero_tensor()
+        report_ctc_loss = zero_tensor()
         report_src_words = zero_tensor()
         report_rec_loss, report_rev_loss, report_mirror_loss = zero_tensor(), zero_tensor(), zero_tensor()
         start = time.time()
@@ -789,6 +806,9 @@ class Trainer(object):
                 report_rev_loss.add_(rev_loss_data)
                 report_mirror_loss.add_(mirror_loss_data)
 
+            if opt.ctc_loss > 0.0:
+                report_ctc_loss.add_(ctc_loss_data)
+
             # control the index a little bit to ensure the log is always printed
             if i == 0 or ((i + 1) % opt.log_interval < self.world_size):
 
@@ -812,6 +832,13 @@ class Trainer(object):
                         log_string += (" rev_ppl: %6.2f ; " % rev_ppl)
                         log_string += (" mir_loss: %6.2f ; " % (report_mirror_loss / report_tgt_words))
 
+                    if opt.ctc_loss > 0.0:
+                        # if torch.isinf(report_ctc_loss):
+                        #     report_ctc_loss.zero_()
+                        # dist.all_reduce(report_ctc_loss, op=dist.ReduceOp.SUM, group=self.group)
+                        ctc_loss = report_ctc_loss.item() / report_tgt_words.item()
+                        log_string += (" ctcloss: %8.2f ; " % ctc_loss)
+
                     log_string += ("lr: %.7f ; updates: %7d; " %
                                    (self.optim.getLearningRate(),
                                     self.optim._step))
@@ -831,6 +858,7 @@ class Trainer(object):
                 report_rec_loss.zero_()
                 report_rev_loss.zero_()
                 report_mirror_loss.zero_()
+                report_ctc_loss.zero_()
                 start = time.time()
 
             # increase i by world size
@@ -844,24 +872,11 @@ class Trainer(object):
         opt = self.opt
 
         if checkpoint is not None:
-            raise NotImplementedError
 
             # TODO: have loading checkpoints for each process
-            self.model.load_state_dict(checkpoint['model'])
             prec_opt = checkpoint['opt'] if 'opt' in checkpoint else None
 
-            opt.reset_optim = True
-
             if not opt.reset_optim:
-
-                if self.is_main():
-                    print("* Loading optimizer states ... ")
-                self.optim.load_state_dict(checkpoint['optim'])
-                if prec_opt is not None and hasattr(prec_opt, "fp16_mixed"):
-                    # Only load amp information if the mode is the same
-                    # Maybe its better to change between optimization mode?
-                    if 'scaler' in checkpoint:
-                        self.grad_scaler.load_state_dict(checkpoint['scaler'])
 
                 # Only load the progress when we use the same optimizer
                 if 'itr' in checkpoint:
@@ -878,9 +893,8 @@ class Trainer(object):
                 resume = False
                 start_epoch = 1
 
-            del checkpoint['model']
-            optim_state_dict = checkpoint['optim']
-            # del checkpoint['optim']
+            # optim_state_dict = checkpoint['optim']
+            # # del checkpoint['optim']
             del checkpoint
         else:
             itr_progress = None

onmt/train_utils/trainer.py
@@ -304,7 +304,6 @@ class XETrainer(BaseTrainer):
         if opt.ctc_loss != 0:
             from onmt.speech.ctc_loss import CTC
             self.ctc_loss_function = CTC(dicts['tgt'].size(), opt.model_size, 0.0, reduce=True)
-            model.ctc_projection = self.ctc_loss_function.projection
 
         if self.cuda:
             torch.cuda.set_device(self.opt.gpus[0])
@@ -312,7 +311,8 @@ class XETrainer(BaseTrainer):
                 torch.manual_seed(self.opt.seed)
             self.loss_function = self.loss_function.cuda()
             self.model = self.model.cuda()
-            self.ctc_loss_function = self.ctc_loss_function.cuda()
+            if opt.ctc_loss > 0.0:
+                self.ctc_loss_function = self.ctc_loss_function.cuda()
 
         if setup_optimizer:
 
@@ -474,6 +474,7 @@ class XETrainer(BaseTrainer):
         total_non_pads = 0
         report_loss, report_tgt_words = 0, 0
         report_src_words = 0
+        report_ctc_loss = 0
         report_rec_loss, report_rev_loss, report_mirror_loss = 0, 0, 0
         start = time.time()
         n_samples = len(epoch_iterator)
@@ -536,6 +537,7 @@ class XETrainer(BaseTrainer):
                     ctc_loss = self.ctc_loss_function(outputs, targets)
                     ctc_loss_data = ctc_loss.item()
                     full_loss = full_loss + opt.ctc_loss * ctc_loss
+                    report_ctc_loss += ctc_loss_data
 
                 if opt.mirror_loss:
                     rev_loss = loss_dict['rev_loss']
@@ -687,6 +689,13 @@ class XETrainer(BaseTrainer):
                                    (report_src_words / (time.time() - start),
                                     report_tgt_words / (time.time() - start)))
 
+                    if opt.ctc_loss > 0.0:
+                        # if torch.isinf(report_ctc_loss):
+                        #     report_ctc_loss.zero_()
+                        # dist.all_reduce(report_ctc_loss, op=dist.ReduceOp.SUM, group=self.group)
+                        ctc_loss = report_ctc_loss / report_tgt_words
+                        log_string += (" ctcloss: %8.2f ; " % ctc_loss)
+
                     log_string += ("%s elapsed" %
                                    str(datetime.timedelta(seconds=int(time.time() - self.start_time))))
 
@@ -695,6 +704,7 @@ class XETrainer(BaseTrainer):
                     report_loss = 0
                     report_tgt_words, report_src_words = 0, 0
                     report_rec_loss, report_rev_loss, report_mirror_loss = 0, 0, 0
+                    report_ctc_loss = 0
                     start = time.time()
 
                 i = i + 1

train_distributed.py
@@ -407,6 +407,8 @@ def main():
     if opt.load_from:
         checkpoint = torch.load(opt.load_from, map_location=lambda storage, loc: storage)
         print("* Loading dictionaries from the checkpoint")
+        del checkpoint['model']
+        del checkpoint['optim']
         dicts = checkpoint['dicts']
     else:
         dicts['tgt'].patch(opt.patch_vocab_multiplier)
@@ -419,8 +421,8 @@ def main():
         print(' * vocabulary size. target = %d' %
               (dicts['tgt'].size()))
 
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '8888'
+    os.environ['MASTER_ADDR'] = opt.master_addr  # default 'localhost'
+    os.environ['MASTER_PORT'] = opt.master_port  # default '8888'
 
     # spawn N processes for N gpus
     # each process has a different trainer