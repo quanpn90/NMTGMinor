Fix EncDecAttn CUDA and softmax backward.

onmt/modules/optimized/test_self_attention_func.py
@@ -124,6 +124,7 @@ class SelfMultiheadAttnTest(unittest.TestCase):
 
     def test_performance(self):
         training = True
+        dropout=0.5
 
         mask = ((torch.randn(self.sequences, self.seq_length) > 0)).bool().cuda()
 
@@ -137,7 +138,7 @@ class SelfMultiheadAttnTest(unittest.TestCase):
                                                       self.ref_parameters.out_proj_weight,
                                                       self.ref_parameters.in_proj_bias,
                                                       self.ref_parameters.out_proj_bias,
-                                                      mask, 0.5,
+                                                      mask, dropout,
                                                       False, None, False, None,
                                                       False, True)
 
@@ -158,7 +159,7 @@ class SelfMultiheadAttnTest(unittest.TestCase):
                                                       self.tst_parameters.out_proj_weight,
                                                       self.tst_parameters.in_proj_bias,
                                                       self.tst_parameters.out_proj_bias,
-                                                      mask, 0.5,
+                                                      mask, dropout,
                                                       False, None, False, None,
                                                       True, True)
 

pretrain_module/modeling_mbart.py
@@ -306,9 +306,9 @@ class MBartAttention(nn.Module):
             hidden_states = hidden_states
             use_time_mask = self.is_decoder
             qlen, klen = hidden_states.size(0), hidden_states.size(0)
-            
+
             mask = attention_mask
-            low_precision = False  # Use CUDA impl
+            low_precision = True  # Use CUDA impl
 
             attn_output, coverage = self_attn_func(use_time_mask, self.training, self.num_heads, hidden_states,
                                                    self.proj_weight, self.out_proj.weight,